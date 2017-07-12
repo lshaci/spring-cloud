5356d826c10743377157a3a8e7e4e2d3857d12b8
lshaci <lshaci@qq.com>
2017-07-12 17:14:06 +0800

    添加注释

--- spring-cloud-config-client/src/main/java/com/lshaci/config/client/ConfigClientApplication.java
+++ spring-cloud-config-client/src/main/java/com/lshaci/config/client/ConfigClientApplication.java
@@ -1,12 +1,12 @@
 package com.lshaci.config.client;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class ConfigClientApplication {
 
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(ConfigClientApplication.class).web(true).run(args);
+		SpringApplication.run(ConfigClientApplication.class, args);
 	}
 }
--- spring-cloud-config-server/src/main/java/com/lshaci/config/server/ConfigServerApplication.java
+++ spring-cloud-config-server/src/main/java/com/lshaci/config/server/ConfigServerApplication.java
@@ -1,7 +1,7 @@
 package com.lshaci.config.server;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 // 开启Config Server
@@ -10,6 +10,6 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 public class ConfigServerApplication {
 	
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(ConfigServerApplication.class).web(true).run(args);
+		SpringApplication.run(ConfigServerApplication.class, args);
 	}
 }
